fb85bb0507ae5b813e27aaaebec577863aec50c4
cloudherz <makar.yanchev@gmail.com>
2025-10-18 18:33:55 +0300

    Alpha 1.0.6.0
    [VISUALS]
    - Updated front page
    
    [FUNCTIONALITY]
    - Minor changes in scripts

---
+++ resources/views/components/blades/buttons/medium.blade.php
@@ -0,0 +1,10 @@
+<a class="L-BUTTON-link" href="{{ $href }}">
+    <div class="S-BUTTON-frame">
+        <p class="T-BUTTON-title TYPO-WEIGHT-SEMIBOLD TYPO-OBLIQUE-NONE TYPO-D-SIZES-NORMAL TYPO-VARIABLE">
+            {!! $title !!}
+        </p>
+        <svg class="I-BUTTON-arrow" viewBox="0 0 17 16" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16.4995 6.49731V9.50269C13.2719 9.50269 10.6462 12.0887 10.6462 15.2676H7.59475C7.59475 13.0627 8.42489 11.045 9.79459 9.50269H0.0263672V6.49731H9.79459C8.42489 4.95499 7.59475 2.93728 7.59475 0.732422H10.6462C10.6462 3.9113 13.2719 6.49731 16.4995 6.49731Z"/>
+        </svg>
+    </div>
+</a>
--- resources/views/pages/generic/home.blade.php
+++ resources/views/pages/generic/home.blade.php
@@ -20,12 +20,47 @@
                         <div class="S-FRONT-content">
                             <div class="S-CONTENT-wrapper">
                                 <div class="S-CONTENT-carcass">
-                                    <div class="S-CONTENT-main">
-                                        <p class="T-CONTENT-text TYPO-WEIGHT-EXTRABOLD TYPO-OBLIQUE-NONE TYPO-D-SIZES-H1 TYPO-VARIABLE">Макарий Янчев</p>
-                                        <p class="T-CONTENT-text TYPO-WEIGHT-MEDIUM TYPO-OBLIQUE-NONE TYPO-D-SIZES-H3 TYPO-VARIABLE">aka <span class="TYPO-WEIGHT-BOLD TYPO-OBLIQUE-NONE TYPO-VARIABLE">sh1ft</span></p>
+                                    <div class="S-CONTENT-name">
+                                        <p class="T-NAME-text TYPO-WEIGHT-EXTRABOLD TYPO-OBLIQUE-NONE TYPO-D-SIZES-H1 TYPO-VARIABLE">
+                                            Макарий Янчев
+                                        </p>
+                                        <p class="T-NAME-text TYPO-WEIGHT-MEDIUM TYPO-OBLIQUE-NONE TYPO-D-SIZES-H3 TYPO-VARIABLE">
+                                            aka
+                                            <span class="TYPO-WEIGHT-BOLD TYPO-OBLIQUE-NONE TYPO-VARIABLE">
+                                                sh1ft
+                                            </span>
+                                        </p>
                                     </div>
-                                    <div class="S-CONTENT-bottom">
-                                        <p class="T-CONTENT-text TYPO-WEIGHT-MEDIUM TYPO-OBLIQUE-NONE TYPO-D-SIZES-NORMAL TYPO-VARIABLE">Что-то интересное</p>
+                                    <div class="S-CONTENT-buttons">
+                                        <x-blades.buttons.medium
+                                            href="/web"
+                                            title="веб-разработка"
+                                        />
+                                        <x-blades.buttons.medium
+                                            href="/gr"
+                                            title="графический дизайн"
+                                        />
+                                        <x-blades.buttons.medium
+                                            href="/3d"
+                                            title="<span class='TYPO-DETAILS-ONUM'>3</span>d-дизайн"
+                                        />
+                                        <x-blades.buttons.medium
+                                            href="/me"
+                                            title="обо мне"
+                                        />
+                                    </div>
+                                    <div class="S-CONTENT-shortcuts">
+                                        <a class="L-SHORTCUT-link TYPO-WEIGHT-MEDIUM TYPO-OBLIQUE-NONE TYPO-D-SIZES-NORMAL TYPO-VARIABLE" href="/fwaeh">Резюме</a>
+                                        <a class="L-SHORTCUT-icon" href="/SIYAAA">
+                                            <svg class="I-SHORTCUT-all I-SHORTCUT-icon_telegram" viewBox="0 0 33 28" xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd" clip-rule="evenodd" d="M2.26805 12.1237C11.1266 8.31284 17.0332 5.80157 19.9888 4.58754C28.4284 1.12239 30.1816 0.520062 31.3246 0.500141C31.5763 0.496625 32.1377 0.557561 32.5022 0.84935C32.8096 1.09544 32.8939 1.42824 32.9342 1.66144C32.9746 1.89581 33.0256 2.42666 32.9853 2.84266C32.5283 7.5863 30.5495 19.0973 29.543 24.4105C29.1168 26.6593 28.2776 27.4128 27.4657 27.4866C25.7018 27.6471 24.3617 26.3347 22.6524 25.2284C19.9781 23.4988 18.4671 22.4207 15.8711 20.7321C12.8703 18.7798 14.8158 17.7075 16.5251 15.9545C16.9726 15.4951 24.7475 8.51323 24.8982 7.88043C24.9172 7.80074 24.935 7.50544 24.757 7.34958C24.5789 7.19373 24.3166 7.24763 24.1279 7.28982C23.8596 7.34958 19.5876 10.1374 11.3106 15.6533C10.0975 16.4759 8.99835 16.8755 8.01432 16.8556C6.9294 16.8322 4.84266 16.2498 3.29125 15.7517C1.38848 15.1412 -0.123755 14.8178 0.00800226 13.7807C0.0756613 13.2405 0.829407 12.6885 2.26805 12.1237Z"/>
+                                            </svg>
+                                        </a>
+                                        <a class="L-SHORTCUT-icon" href="/BALENCIAGA">
+                                            <svg class="I-SHORTCUT-all I-SHORTCUT-icon_email" viewBox="0 0 33 22" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M2.475 0C2.15079 0 1.84303 0.06985 1.55977 0.185096L15.559 12.732C16.1641 13.2742 16.8087 13.2746 17.4152 12.732L31.4402 0.185096C31.157 0.06985 30.8492 0 30.525 0H2.475ZM0.0257812 2.18146C0.0101764 2.29713 0 2.41827 0 2.53846V19.4615C0 20.8678 1.10387 22 2.475 22H30.525C31.8961 22 33 20.8678 33 19.4615V2.53846C33 2.41827 32.9898 2.29713 32.9742 2.18146L19.0395 14.649C17.5856 15.9496 15.3885 15.9517 13.9348 14.649L0.0257812 2.18146Z"/>
+                                            </svg>
+                                        </a>
                                     </div>
                                 </div>
                             </div>
