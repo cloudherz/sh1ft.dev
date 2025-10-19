76d4e82755ae7bd29486fe2d3d81598538b4f859
cloudherz <makar.yanchev@gmail.com>
2025-10-19 17:22:35 +0300

    Alpha 1.0.7.0
    [VISUALS]
    - Created a skills section and corresponding components

---
+++ resources/views/components/blades/skills/card.blade.php
@@ -0,0 +1,17 @@
+<div class="S-SKILLS-{{ $categoryName }} S-SKILLS-all">
+    <div class="S-ALL-wrapper">
+        <div class="S-ALL-carcass">
+            <div class="S-ALL-title">
+                {!! $categoryIcon !!}
+                <p class="T-ALL-title TYPO-WEIGHT-SEMIBOLD TYPO-OBLIQUE-NONE TYPO-D-SIZES-NORMAL TYPO-VARIABLE">{{ $categoryTitle }}</p>
+            </div>
+            <div class="S-ALL-tags">
+                <div class="S-TAGS-wrapper">
+                    <div class="S-TAGS-carcass">
+                        {{ $slot }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/blades/tags/tag.blade.php
@@ -0,0 +1,12 @@
+<div class="S-TAGS-tag">
+    <div class="S-TAG-wrapper">
+        <div class="S-TAG-carcass">
+            <div class="S-TAG-icon">
+                {!! $icon !!}
+            </div>
+            <div class="S-TAG-title">
+                <p class="T-TAG-title TYPO-WEIGHT-SEMIBOLD TYPO-OBLIQUE-NONE TYPO-D-SIZES-NORMAL TYPO-VARIABLE">{{ $title }}</p>
+            </div>
+        </div>
+    </div>
+</div>
